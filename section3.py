1e052bae0bd2ab976fe5f75054740b847340da1e
Kimeunhye <ses8467@naver.com>
2015-10-31 15:46:43 +0900

    third commit

---
+++ section3.py
@@ -0,0 +1,61 @@
+# 3.1
+brian = '언제나 삶의 긍정적인 면을 살피세요!'
+
+# 3.2
+caesar = "Graham"
+praline = "John"
+viking = "Teresa"
+
+# 3.3
+'Help! Help! I\'m being repressed!'
+
+# 3.4
+fifth_letter = "MONTY"[4]
+
+# 3.5
+parrot = "Norwegian Blue"
+print len(parrot)
+
+# 3.6
+parrot = "Norwegian Blue"
+print parrot.lower()
+
+# 3.7
+parrot = "norwegian blue"
+print parrot.upper()
+
+# 3.8
+pi = 3.14
+print str(pi)
+
+# 3.9
+ministry = "The Ministry of Silly Walks"
+
+print len(ministry)
+print ministry.upper()
+
+# 3.10
+print "Monty Python"
+
+# 3.11
+the_machine_goes = "Ping!"
+print the_machine_goes
+
+# 3.12
+print "Spam " + "and " + "eggs"
+
+# 3.13
+print "The value of pi is around " + str(3.14)
+
+# 3.14
+name = raw_input("What is your name?")
+quest = raw_input("What is your quest?")
+color = raw_input("What is your favorite color?")
+
+print "Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color)
+
+# 3.15
+my_string = "Manofin"
+print len(my_string)
+print my_string.upper()
