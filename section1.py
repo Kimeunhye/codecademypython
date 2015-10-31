09e7ac9e3e0608b0e5ffffdfac73832cc5b79b14
Kimeunhye <ses8467@naver.com>
2015-10-31 14:35:28 +0900

    first commit

---
+++ section1.py
@@ -0,0 +1,45 @@
+# 1.1
+print "Welcome to Python!"
+
+# 1.2
+my_variable = 10
+
+# 1.3
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+# 1.4
+my_int = 3
+
+# 1.5
+def spam():
+    eggs = 12
+    return eggs
+
+print spam()
+
+# 1.6
+spam = True
+eggs = False
+
+# 1.7
+#mysterious_variable = 42
+
+# 1.8
+"""저는 나무꾼입니다."""
+
+# 1.9
+count_to = 1+2
+
+# 1.10
+eggs = 10 ** 2
+
+# 1.11
+spam = 3 % 2
+
+# 1.12
+# codecademy start
+monty = True
+python = 1.234
+monty_python = python ** 2
