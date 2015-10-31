39fe7d50bb43f144bdd752fb56fb16c5c18ce815
Kimeunhye <ses8467@naver.com>
2015-10-31 14:55:06 +0900

    second commit

---
+++ section2.py
@@ -0,0 +1,28 @@
+# 2.1
+meal = 44.50
+
+# 2.2
+meal = 44.50
+tax = 0.0675
+
+# 2.3
+meal = 44.50
+tax = 0.0675
+tip = 0.15
+
+# 2.4
+meal = 44.50
+tax = 0.0675
+tip = 0.15
+
+meal = meal + meal * tax
+
+# 2.5
+meal = 44.50
+tax = 0.0675
+tip = 0.15
+
+meal = meal + meal * tax
+total = meal + meal*tip
+
+print("%.2f" % total)
